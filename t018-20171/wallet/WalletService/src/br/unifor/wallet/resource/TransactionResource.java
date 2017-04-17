28e1ef1425bc0bba24d95a907b8827fbeb8ee1d8
Levi Rocha <ilevidr@gmail.com>
2017-04-17 20:29:02 -0300

    created service

---
+++ t018-20171/wallet/WalletService/src/br/unifor/wallet/resource/TransactionResource.java
@@ -0,0 +1,60 @@
+package br.unifor.wallet.resource;
+
+import java.util.Collection;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import br.unifor.wallet.bo.TransactionBO;
+import br.unifor.wallet.entity.Transaction;
+
+@Stateless
+@Path("/transactions")
+public class TransactionResource {
+
+	@EJB
+	private TransactionBO transactionBO;
+
+	@GET
+	@Path("{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Transaction findById(@PathParam("id") Long id) {
+		return transactionBO.findById(id);
+	}
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Collection<Transaction> list() {
+		return transactionBO.list();
+	}
+
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public String insert(Transaction transaction) {
+		return transactionBO.insert(transaction);
+	}
+
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String update(Transaction transaction) {
+		return transactionBO.update(transaction);
+	}
+
+	@DELETE
+	@Path("{id}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String remove(@PathParam("id") Long id) {
+		return transactionBO.remove(id);
+	}
+
+}
